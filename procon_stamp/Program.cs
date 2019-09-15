c0dd71f0f6e2de47fb9d800d51273f7ea8129824
igarashi339 <igarashi339@gmail.com>
2019-09-15 18:14:38 +0900

    リポジトリの作成

---
+++ procon_stamp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace procon_stamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
