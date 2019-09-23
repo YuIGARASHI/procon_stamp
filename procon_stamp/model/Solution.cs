e33392dd5899148dab2e8232695e6620845c8beb
igarashi339 <igarashi339@gmail.com>
2019-09-23 15:31:59 +0900

    Solutionクラスを実装

--- procon_stamp/model/Solution.cs
+++ procon_stamp/model/Solution.cs
@@ -6,5 +6,25 @@ using System.Threading.Tasks;
 
 namespace procon_stamp.model {
     class Solution {
+        // 解を表す3-tupleのリスト
+        private List<Tuple<Stamp, int, int>> stamp_answer_list;
+
+        public Solution()
+        {
+            this.stamp_answer_list = new List<Tuple<Stamp, int, int>>();
+        }
+
+        // 解に要素を追加する
+        public void AddStampAnswer(Stamp stamp_object, int x, int y)
+        {
+            this.stamp_answer_list.Add(new Tuple<Stamp, int, int>(stamp_object, x, y));
+            return;
+        }
+
+        // 解を取得する
+        public List<Tuple<Stamp, int ,int>> GetStampAnswerList()
+        {
+            return this.stamp_answer_list;
+        }
     }
 }
