eedebd7e1ac4dfa9eff2b76b8a1e2263cc036433
igarashi339 <igarashi339@gmail.com>
2019-09-29 15:41:00 +0900

    ・変数の型ををintからshortに変更 ・Shapeクラスにハートを追加

--- experimnt/Program.cs
+++ experimnt/Program.cs
@@ -11,29 +11,29 @@ namespace experimnt
     {
         static void Main(string[] args)
         {
-            const int field_x_size = 100;
-            const int field_y_size = 100;
+            const short field_x_size = 100;
+            const short field_y_size = 100;
 
-            Stamp stamp = new Stamp(Shape.star);
+            Stamp stamp = new Stamp(Shape.heart);
             Field field = new Field(field_x_size, field_y_size);
 
             System.Random r = new System.Random(1000);
-            for ( int count = 0; count < 1000000; ++count )
+            for ( short count = 0; count < 100; ++count )
             {
-                int slide_x = r.Next(stamp.x_size * (-1), field_x_size);
-                int slide_y = r.Next(stamp.y_size * (-1), field_y_size);
+                short slide_x = (short)r.Next(stamp.x_size * (-1), field_x_size);
+                short slide_y = (short)r.Next(stamp.y_size * (-1), field_y_size);
                 field.PressStamp(stamp, slide_x, slide_y);
 
 
                 // 区切り線の出力
                 Console.WriteLine("\n\n\n\n\n");
-                for (int i = 0; i < field_x_size; ++i)
+                for (short i = 0; i < field_x_size; ++i)
                 {
                     Console.Write(" >");
                 }
                 Console.WriteLine("\n\n\n\n\n");
 
-                field.PrintMyField();
+                field.PrshortMyField();
             }
         }
     }
@@ -45,22 +45,25 @@ namespace experimnt
 
         // 縦14,横10の正方形
         public static string rect = "27;20;000000000000000000000000000000000000000000000000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000011111111110000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+
+        // ハート
+        public static string heart = "64;64;0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001111000000000000000000000000001111000000000000000000000000001111111111110000000000000000001111111111110000000000000000000011111111111111110000000000000011111111111111110000000000000000011111111111111111110000000000011111111111111111100000000000000011111111111111111111100000000111111111111111111111000000000000011111111111111111111111000000111111111111111111111110000000000011111111111111111111111110000111111111111111111111111100000000011111111111111111111111111100111111111111111111111111111000000001111111111111111111111111110011111111111111111111111111100000000111111111111111111111111111111111111111111111111111111111000000111111111111111111111111111111111111111111111111111111111100000011111111111111111111111111111111111111111111111111111111110000001111111111111111111111111111111111111111111111111111111111000001111111111111111111111111111111111111111111111111111111111110000111111111111111111111111111111111111111111111111111111111111000011111111111111111111111111111111111111111111111111111111111100001111111111111111111111111111111111111111111111111111111111110000111111111111111111111111111111111111111111111111111111111111000011111111111111111111111111111111111111111111111111111111111100001111111111111111111111111111111111111111111111111111111111110000011111111111111111111111111111111111111111111111111111111110000001111111111111111111111111111111111111111111111111111111111000000111111111111111111111111111111111111111111111111111111111100000011111111111111111111111111111111111111111111111111111111110000000111111111111111111111111111111111111111111111111111111110000000011111111111111111111111111111111111111111111111111111111000000001111111111111111111111111111111111111111111111111111111100000000011111111111111111111111111111111111111111111111111111100000000001111111111111111111111111111111111111111111111111111110000000000011111111111111111111111111111111111111111111111111110000000000001111111111111111111111111111111111111111111111111111000000000000011111111111111111111111111111111111111111111111111000000000000000111111111111111111111111111111111111111111111111000000000000000011111111111111111111111111111111111111111111111100000000000000000111111111111111111111111111111111111111111111100000000000000000001111111111111111111111111111111111111111111100000000000000000000111111111111111111111111111111111111111111110000000000000000000001111111111111111111111111111111111111111110000000000000000000000011111111111111111111111111111111111111110000000000000000000000000111111111111111111111111111111111111110000000000000000000000000001111111111111111111111111111111111110000000000000000000000000000011111111111111111111111111111111110000000000000000000000000000000111111111111111111111111111111110000000000000000000000000000000001111111111111111111111111111110000000000000000000000000000000000001111111111111111111111111100000000000000000000000000000000000000011111111111111111111111100000000000000000000000000000000000000000111111111111111111111100000000000000000000000000000000000000000000111111111111111111000000000000000000000000000000000000000000000000111111111111110000000000000000000000000000000000000000000000000001111111111110000000000000000000000000000000000000000000000000000001111111100000000000000000000000000000000000000000000000000000000001111000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
     }
 
     class Field
     {
         private bool[,] my_filed;
-        private int x_size;
-        private int y_size;
+        private short x_size;
+        private short y_size;
 
-        public Field(int x_size, int y_size)
+        public Field(short x_size, short y_size)
         {
             this.x_size = x_size;
             this.y_size = y_size;
             this.my_filed = new bool[y_size, x_size];
-            for ( int y_ind = 0; y_ind < y_size; ++y_ind )
+            for ( short y_ind = 0; y_ind < y_size; ++y_ind )
             {
-                for ( int x_ind = 0; x_ind < x_size; ++x_ind )
+                for ( short x_ind = 0; x_ind < x_size; ++x_ind )
                 {
                     this.my_filed[y_ind, x_ind] = false;
                 }
@@ -68,12 +71,12 @@ namespace experimnt
         }
 
         // stamp をスライドしてからを my field に押す
-        public void PressStamp(Stamp stamp, int x_slide, int y_slide)
+        public void PressStamp(Stamp stamp, short x_slide, short y_slide)
         {
-            foreach ( var coordinate in stamp.GetBlackCellCoordinates() )
+            foreach ( Tuple<short, short> coordinate in stamp.GetBlackCellCoordinates() )
             {
-                int y = y_slide + coordinate.Item1;
-                int x = x_slide + coordinate.Item2;
+                short y = (short)(y_slide + coordinate.Item1);
+                short x = (short)(x_slide + coordinate.Item2);
                 
                 // スタンプを押す場所が my field の外なら continue
                 if ( y < 0 || y >= this.y_size || x < 0 || x >= this.x_size)
@@ -86,11 +89,11 @@ namespace experimnt
         }
 
         // my fieldを標準出力に表示する
-        public void PrintMyField()
+        public void PrshortMyField()
         {
-            for (int y_ind = 0; y_ind < y_size; ++y_ind)
+            for (short y_ind = 0; y_ind < y_size; ++y_ind)
             {
-                for (int x_ind = 0; x_ind < x_size; ++x_ind)
+                for (short x_ind = 0; x_ind < x_size; ++x_ind)
                 {
                     if ( this.my_filed[y_ind, x_ind] )
                     {
@@ -105,12 +108,12 @@ namespace experimnt
             }
         }
 
-        public int GetBlackCellNum()
+        public short GetBlackCellNum()
         {
-            int count = 0;
-            for ( int y_ind = 0; y_ind < this.y_size; ++y_ind )
+            short count = 0;
+            for ( short y_ind = 0; y_ind < this.y_size; ++y_ind )
             {
-                for ( int x_ind = 0; x_ind < this.x_size; ++x_ind )
+                for ( short x_ind = 0; x_ind < this.x_size; ++x_ind )
                 {
                     if (this.my_filed[y_ind, x_ind]) count++;
                 }
@@ -122,32 +125,32 @@ namespace experimnt
 
     class Stamp
     {
-        private List<Tuple<int, int>> black_cell_coordinates;
-        public int x_size;
-        public int y_size;
+        private List<Tuple<short, short>> black_cell_coordinates;
+        public short x_size;
+        public short y_size;
 
         public Stamp(string input_str)
         {
             string[] input_array = input_str.Split(';');
-            this.x_size = int.Parse(input_array[0]);
-            this.y_size = int.Parse(input_array[1]);
+            this.x_size = short.Parse(input_array[0]);
+            this.y_size = short.Parse(input_array[1]);
 
-            this.black_cell_coordinates = new List<Tuple<int, int>>();
-            int input_array_idx = 0;
-            for ( int y_idx = 0; y_idx < this.y_size; ++y_idx )
+            this.black_cell_coordinates = new List<Tuple<short, short>>();
+            short input_array_idx = 0;
+            for ( short y_idx = 0; y_idx < this.y_size; ++y_idx )
             {
-                for ( int x_idx = 0; x_idx < this.x_size; ++x_idx )
+                for ( short x_idx = 0; x_idx < this.x_size; ++x_idx )
                 {
                     if ( input_array[2][input_array_idx++] == '1' )
                     {
-                        this.black_cell_coordinates.Add(new Tuple<int, int>(y_idx, x_idx));
+                        this.black_cell_coordinates.Add(new Tuple<short, short>(y_idx, x_idx));
                     }
                 }
             }
 
         }
 
-        public List<Tuple<int, int>> GetBlackCellCoordinates()
+        public List<Tuple<short, short>> GetBlackCellCoordinates()
         {
             return this.black_cell_coordinates;
         }
