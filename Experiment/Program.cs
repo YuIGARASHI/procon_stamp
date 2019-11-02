b4ba52f8528ed49255f7d19b758d5dc93870564f
igarashi339 <igarashi339@gmail.com>
2019-11-03 01:42:36 +0900

    google or-tools をインストール

--- Experiment/Program.cs
+++ Experiment/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using StampLib.algorithm;
 using StampLib.model;
 using StampLib.util;
+using Google.OrTools.LinearSolver;
 
 namespace Experiment
 {
@@ -14,7 +15,7 @@ namespace Experiment
         static void Main(string[] args)
         {
             var experiment = new ExperimentsIgarashi();
-            experiment.PrintSomeInstances();
+            experiment.OrToolsSimpleLpProgram();
         }
     }
 
@@ -57,6 +58,42 @@ namespace Experiment
                     }
                 }
             }
+        }
+
+        /// <summary>
+        /// Google or-toolsのサンプルプログラム
+        /// https://developers.google.com/optimization/introduction/dotnet?hl=ja
+        /// </summary>
+        public void OrToolsSimpleLpProgram()
+        {
+            // Create the linear solver with the GLOP backend.
+            Solver solver = Solver.CreateSolver("SimpleLpProgram", "GLOP_LINEAR_PROGRAMMING");
+
+            // Create the variables x and y.
+            Variable x = solver.MakeNumVar(0.0, 1.0, "x");
+            Variable y = solver.MakeNumVar(0.0, 2.0, "y");
+
+            Console.WriteLine("Number of variables = " + solver.NumVariables());
+
+            // Create a linear constraint, 0 <= x + y <= 2.
+            Constraint ct = solver.MakeConstraint(0.0, 2.0, "ct");
+            ct.SetCoefficient(x, 1);
+            ct.SetCoefficient(y, 1);
+
+            Console.WriteLine("Number of constraints = " + solver.NumConstraints());
+
+            // Create the objective function, 3 * x + y.
+            Objective objective = solver.Objective();
+            objective.SetCoefficient(x, 3);
+            objective.SetCoefficient(y, 1);
+            objective.SetMaximization();
+
+            solver.Solve();
+
+            Console.WriteLine("Solution:");
+            Console.WriteLine("Objective value = " + solver.Objective().Value());
+            Console.WriteLine("x = " + x.SolutionValue());
+            Console.WriteLine("y = " + y.SolutionValue());
         }
     }
 
